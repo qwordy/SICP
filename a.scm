2133c0fc53d613c3af832aa3e492ec9871dc0339
qwordy <iamyfy@163.com>
2015-12-01 22:45:26 +0800

    qwordy

--- a.scm
+++
@@ -1,4 +0,0 @@
-#lang racket
-(display 1)
-(newline)
-
--- pic.scm
+++
@@ -1,7 +0,0 @@
-#lang racket  ; A picture
-(require 2htdp/image)
-(let sierpinski ([n 8])
-  (cond
-    [(zero? n) (triangle 2 'solid 'red)]
-    [else (define t (sierpinski (- n 1)))
-          (freeze (above t (beside t t)))]))
